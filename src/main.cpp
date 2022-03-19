a3c4779b1a0641e31a2c61b776fdacb4154675b2
Kaustubh Khare <kkhare@node-0.kkhare-120825.advosuwmadison.emulab.net>
2022-03-19 15:08:39 -0600

    basic agg code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <mpi.h>
+#include <unistd.h>
+#include "mpi_helper.h"
 
-int main() {
+class SimpleAgg {
+    template <class T>
+    void run(T* v, const int n, const int root, MPI_Comm comm);
+};
 
+int main (int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+    MPIHelper::init(MPI_COMM_WORLD);
+
+    char hostname[HOST_NAME_MAX + 1];
+    gethostname(hostname, HOST_NAME_MAX + 1);
+
+    std::cerr << hostname << " " << argc << " -> ";
+    for (int i = 0; i < argc; i++)
+        std::cerr << argv[i] << " | ";
+    std::cout << "\n";
+
+
+    MPI_Finalize();
 }
