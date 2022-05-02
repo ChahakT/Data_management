37e0fd48df04a44ddd2f1070285afbfb92bbfb57
agarwal722 <95115160+agarwal722@users.noreply.github.com>
2022-05-02 14:49:29 -0500

    Update run_agg.sh

--- run_agg.sh
+++ run_agg.sh
@@ -4,5 +4,5 @@ arr=("SIMPLE_AGG" "SMART_AGG" "SMART_AGG_V2")
 mkdir ../output/
 
 for f in ${arr[@]}; do
-    cmake -D_RUNTYPE=$f -D_BETA=2.0 -D_INTERSECT_V=0 ../ && make -j && ../mpi-install-script.sh mpi-agg-benchmark 2> ../output/err_${f}.txt > ../output/out_${f}.csv
+    cmake -D_RUNTYPE=$f -D_BETA=$1 -D_INTERSECT_V=0 ../ && make -j && ../mpi-install-script.sh mpi-agg-benchmark 2> ../output/err_${f}.txt > ../output/out_${f}.csv
 done
