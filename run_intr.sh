de572b227b7f51b310625ef97d4d6f553681277a
Kaustubh Khare <kharekaustubh10@gmail.com>
2022-05-02 22:03:54 -0500

    Adds run intersect script

--- run_intr.sh
+++ run_intr.sh
@@ -5,10 +5,10 @@ mkdir -p ../output/
 
 for f in ${arr[@]}; do
     (cmake -D_RUNTYPE=$f -D_BETA=58.9 -D_INTERSECT_V=0 ../ && make -j) &> ../output/build_0_${f}.txt
-    ../mpi-install-script.sh mpi-agg-benchmark 2> ../output/err_0_${f}.txt > ../output/out_0_${f}.csv
+    ../mpi-install-script.sh mpi-intersect-benchmark 2> ../output/err_0_${f}.txt > ../output/out_0_${f}.csv
 done
 
 for f in ${arr[@]}; do
     (cmake -D_RUNTYPE=$f -D_BETA=58.9 -D_INTERSECT_V=1 ../ && make -j) &> ../output/build_1_${f}.txt
-    ../mpi-install-script.sh mpi-agg-benchmark 2> ../output/err_1_${f}.txt > ../output/out_1_${f}.csv
+    ../mpi-install-script.sh mpi-intersect-benchmark 2> ../output/err_1_${f}.txt > ../output/out_1_${f}.csv
 done
