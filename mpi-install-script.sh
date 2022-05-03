824595880defa8496e2dd08d91c1c21fd69cf6cb
Kaustubh Khare <kharekaustubh10@gmail.com>
2022-05-02 22:46:23 -0500

    add barrier in intersect test

--- mpi-install-script.sh
+++ mpi-install-script.sh
@@ -8,14 +8,9 @@ mpi-install() {
 
 cmake-install() {
   sudo apt purge --auto-remove cmake
-  sudo apt update &&
-    sudo apt install -y software-properties-common lsb-release &&
-    sudo apt clean all
+  sudo apt update && sudo apt install -y software-properties-common lsb-release && sudo apt clean all
   wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
   sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
-  sudo apt update
-  sudo apt install kitware-archive-keyring -y
-  sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
   sudo apt update
   sudo apt install cmake -y
 }
